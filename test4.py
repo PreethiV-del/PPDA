82a094e4e0f79a796344639a4eb5b2668dc8fb2a
PreethiV-del <preethivbca24@rvu.edu.in>
2025-01-27 15:53:18 +0530

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,7 @@
+x=int(input("Give some number:"))
+if x>0:
+   print("Number is Positive")
+elif x<0:
+   print("Number is Negative")
+else:
+   print("Number is zero")
---
+++ test6.py
@@ -0,0 +1,9 @@
+G=int(input("Enter your grade: "))
+if G>89 and G<=100:
+    print("A")
+elif G>79 and G<=89:
+    print("B")
+elif G>=69 and G<=79:
+    print("C")
+else:
+    print("F")
